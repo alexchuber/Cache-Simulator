e7a427bcc99b41b4fa0026b5d95e41bf10d477de
Alex <91097647+alexchuber@users.noreply.github.com>
2022-06-24 21:37:44 -0400

    Update CacheSimulator.cpp

--- src/CacheSimulator.cpp
+++ src/CacheSimulator.cpp
@@ -15,34 +15,32 @@ public:
     //Constructor
     CacheSimulator(string file, int sc, int bc, int bs, string rp)
     {
-        if (rp == "LRU")
-            cache = new LRUCache(sc, bc, bs);
-        else
-            cache = new FIFOCache(sc, bc, bs);
+	if (rp == "LRU") cache = new LRUCache(sc, bc, bs);
+	else cache = new FIFOCache(sc, bc, bs);
 
-	    infile.open(file);
+	infile.open(file);
 
-        int i = 0;
-	    string line;
-	    while (getline(infile, line) && i < 10000)
-	    {
-            string address = ParseLine(line);
-            cache->Access(address);
-            i++;
-	    }
+	int i = 0;
+	string line;
+	while (getline(infile, line) && i < 10000)
+	{
+	string address = ParseLine(line);
+	cache->Access(address);
+	i++;
+	}
     }
 
     //Destructor
     ~CacheSimulator()
     {
-	    infile.close();
+	infile.close();
     }
 
     // Parameters:  None
     // Return:      pair<INT number of total hits, INT number of otal accesses>
     pair<int, int> GetSummaryStats()
     {
-        return { cache->GetHitCount(), cache->GetAccessCount() };
+    	    return { cache->GetHitCount(), cache->GetAccessCount() };
     }
     
     // Parameters:  STRING line of .trace file
@@ -62,73 +60,41 @@ public:
     // Return:      STRING binary representation of given hex
     string HexToBinary(string hex)
     {
-        cout << "Accessing address " << hex;
+       	    cout << "Accessing address " << hex;
 	    //Remove 0x from beginning
-	    hex = hex.substr(2, hex.length()-2);
+    	    hex = hex.substr(2, hex.length()-2);
 
 	    //Convert each character of remaining string
-        string binary = "";
+            string binary = "";
 	    int i = 0;
 	    while (hex[i])
 	    {
-		    switch (hex[i])
-		    {
-                case '0':
-                    binary += "0000";
-                    break;
-                case '1':
-                    binary += "0001";
-                    break;
-                case '2':
-                    binary += "0010";
-                    break;
-                case '3':
-                    binary += "0011";
-                    break;
-                case '4':
-                    binary += "0100";
-                    break;
-                case '5':
-                    binary += "0101";
-                    break;
-                case '6':
-                    binary += "0110";
-                    break;
-                case '7':
-                    binary += "0111";
-                    break;
-                case '8':
-                    binary += "1000";
-                    break;
-                case '9':
-                    binary += "1001";
-                    break;
-                case 'A':
-                case 'a':
-                    binary += "1010";
-                    break;
-                case 'B':
-                case 'b':
-                    binary += "1011";
-                    break;
-                case 'C':
-                case 'c':
-                    binary += "1100";
-                    break;
-                case 'D':
-                case 'd':
-                    binary += "1101";
-                    break;
-                case 'E':
-                case 'e':
-                    binary += "1110";
-                    break;
-                case 'F':
-                case 'f':
-                    binary += "1111";
-                    break;
-		    }
-		    i++;
+		switch (hex[i])
+		{
+			case '0': binary += "0000"; break;
+			case '1': binary += "0001"; break;
+			case '2': binary += "0010"; break;
+			case '3': binary += "0011"; break;
+			case '4': binary += "0100"; break;
+			case '5': binary += "0101"; break;
+			case '6': binary += "0110"; break;
+			case '7': binary += "0111"; break;
+			case '8': binary += "1000"; break;
+			case '9': binary += "1001"; break;
+			case 'A':
+			case 'a': binary += "1010"; break;
+			case 'B':
+			case 'b': binary += "1011"; break;
+			case 'C':
+			case 'c': binary += "1100"; break;
+			case 'D':
+			case 'd': binary += "1101"; break;
+			case 'E':
+			case 'e': binary += "1110"; break;
+			case 'F':
+			case 'f': binary += "1111"; break;
+		}
+	        i++;
 	    }
 
         cout << " as -->  " << binary << endl;
